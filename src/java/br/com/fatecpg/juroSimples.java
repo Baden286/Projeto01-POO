0f14edfdc14b165002ef62894ddab7482289c86e
delamano <delamano@live.com>
2018-08-25 05:29:19 -0300

    Update Juros Simples

--- src/java/br/com/fatecpg/juroSimples.java
+++ src/java/br/com/fatecpg/juroSimples.java
@@ -37,31 +37,36 @@ public class juroSimples extends HttpServlet {
             out.println("<center>");
             out.println("<br>");
             out.println("<h3>Juros Simples</h3>");
-            out.println("<br><hr>");
+            out.println("<br><hr style='border-color: #28a745'>");
          
-             out.println("<table border='1'>");
+            
+            
+             out.println("<table style='width: 50%' border='3'>");
 
              out.println("<form>");
                       out.println("<tr>");
-             out.println("<td>Capital (R$):</td> "+"<td><input type='number' step='0.01' name='c' maxlength='20' placeholder='ex:2500,25'></td>"); 
+             out.println("<td>Capital (R$):</td> "+"<td><input class='w-100' type='number' step='0.01' name='c' maxlength='20' placeholder='ex:2500,25'></td>"); 
                       out.println("</tr>");
                      
                       out.println("<tr>");
-             out.println("<td>Taxa de Juros (%):</td> "+"<td><input type='number'  step='0.01' name='t' maxlength='20' placeholder='ex:15,5'></td>"); 
+             out.println("<td>Taxa de Juros (%):</td> "+"<td><input class='w-100' type='number'  step='0.01' name='t' maxlength='20' placeholder='ex:15,5'></td>"); 
                       out.println("</tr>");
                       
                       out.println("<tr>");
-             out.println("<td>Número de Períodos (Meses):</td> "+"<td><input type='number' name='i' maxlength='20' placeholder='ex:12'></td>");
+             out.println("<td>Número de Períodos (Meses):</td> "+"<td><input class='w-100' type='number' name='i' maxlength='20' placeholder='ex:12'></td>");
                       out.println("</tr>");
                       
-                      out.println("<tr>");
-             out.println("<td><input type='submit' value='Calcular'><input type='button' value='Voltar' onclick=\"location.href='index.html'\"/></td>"); 
+                      out.println("</table>");
+                      
+                      out.println("<br>");
+                      
+             out.println("<td><input class='btn btn-success font-weight-bold mt-1 mr-5' type='submit' value='Calcular'><input class='btn btn-success font-weight-bold mt-1 mr-3' type='button' value='Voltar' onclick=\"location.href='index.html'\"/></td>"); 
                       out.println("<tr>");
 
              out.println("</form>");
-             out.println("</table>");
+            
 
-            out.println("<br><hr>");
+            out.println("<br><hr style='border-color: #28a745'>");
           
              double c = Double.parseDouble(request.getParameter("c"));
              double i = Double.parseDouble(request.getParameter("i"));
